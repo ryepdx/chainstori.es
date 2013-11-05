e6c3b213b93513f46ba295a0f28a8ba8b6a8c052
Ryan Casey <ryepdx@gmail.com>
2013-11-05 09:15:31 -0800

    Added  login check on the homepage.

--- home/get.py
+++ home/get.py
@@ -1,7 +1,8 @@
 import flask
+from flask.ext.login import current_user
 
 def machine(request):
-    return {}
+    return {"logged_in": current_user.is_authenticated() }
 
 def human(request):
     return flask.render_template("home.html", **machine(request))
