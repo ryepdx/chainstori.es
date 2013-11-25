b0e1965585ba24ad3ffea1fd7560283fa63f404d
Ryan Casey <ryepdx@gmail.com>
2013-11-24 16:30:35 -0800

    Got Flask-Login to take notice of the 'user_loader' function in user/__init__.py.

--- chainstories.py
+++ chainstories.py
@@ -1,6 +1,5 @@
-import cyborg
-import sys
 import settings
+
 from flask import Flask, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
@@ -10,6 +9,12 @@ app.config.from_object(settings)
 app.secret_key = settings.SECRET_KEY
 db = SQLAlchemy(app)
 login_manager = LoginManager(app = app)
+
+# TODO: Clean this up. It's necessary for these imports to happen after
+# the ones above, but it's messy.
+import cyborg
+import sys
+import user
 
 cyborg.Cyborg(sys.modules[__name__]).setup(app)
 
